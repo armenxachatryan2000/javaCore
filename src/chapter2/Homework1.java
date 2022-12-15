17ab7fb867ab907132ac71217f50d245f23b10d4
User <axachatryan877@gmail.com>
2022-12-15 18:03:41 +0400

    "#"

--- src/chapter2/Homework1.java
+++ src/chapter2/Homework1.java
@@ -2,13 +2,15 @@ package chapter2;
 
 public class Homework1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-            }
-        }
+        for (int i = 0; i < 5; i++) {
 
-
+            for (int j = 0; j < i; j++)
+                System.out.print("#");
+            {
+                {
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
