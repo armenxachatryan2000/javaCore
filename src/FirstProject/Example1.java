d3dea6ef8262a07395e23d4a7e6beee226611b35
User <axachatryan877@gmail.com>
2022-12-18 22:40:38 +0400

    Example4

---
+++ src/FirstProject/Example1.java
@@ -0,0 +1,11 @@
+package FirstProject;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 2345;
+        int c = a * b;
+        System.out.println(c);
+    }
+
+}
---
+++ src/chapter2/iforExample.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class iforExample {
+    public static void main(String[] args) {
+        for (int i= 0; i<5 ;i++) {
+
+            for (int j = 0; j <=i ; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
