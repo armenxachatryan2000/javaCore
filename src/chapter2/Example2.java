d6478399fffbbbe499c8c6d8d6043ca873699f7c
User <axachatryan877@gmail.com>
2022-12-18 23:29:42 +0400

    Example2

---
+++ src/chapter2/Example2.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int a, b;
+        a = 65;
+        b = 65;
+        if (a == b) System.out.println(a = b);
+
+
+    }
+}
