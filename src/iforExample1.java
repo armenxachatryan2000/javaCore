9d3df5b9f5c174aa2897c162f7962e62fe833927
User <axachatryan877@gmail.com>
2022-12-19 00:36:05 +0400

    iforExample1.java

---
+++ src/iforExample1.java
@@ -0,0 +1,14 @@
+public class iforExample1 {
+    public static void main(String[] args) {
+        int x = 5;
+
+
+        for (int i = x; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+  "");
+            }
+            System.out.println();
+
+        }
+    }
+}
