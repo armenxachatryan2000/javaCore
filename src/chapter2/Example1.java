28090b5cf8c98d8dc49c7104a3e028defdffa2de
User <axachatryan877@gmail.com>
2022-12-18 23:36:50 +0400

    Example1

--- src/chapter2/Example1.java
+++ src/chapter2/Example1.java
@@ -2,12 +2,9 @@ package chapter2;
 
 public class Example1 {
     public static void main(String[] args) {
-        int a=10;
+        int a= 10;
         int b=40;
-
-        System.out.println("max"+ 40);
-
+        System.out.println("max40");
     }
+
 }
-
-
