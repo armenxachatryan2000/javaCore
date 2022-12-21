83713f3ddbf13cceadc8dcc2b8df6becc63e5ae7
User <axachatryan877@gmail.com>
2022-12-21 23:11:10 +0400

    ArrayUtill.java

---
+++ src/Homework/ArrayUtill.java
@@ -0,0 +1,18 @@
+package Homework;
+
+public class ArrayUtill {
+    public static void main(String[] args) {
+
+        int[] array = {5, 8, 36, 1, 2, 89, 6, 57,};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+            
+
+        }
+
+    }
+}
+
+
+
