e9ca8da2999729e52ad00f66fed58c051704129e
User <axachatryan877@gmail.com>
2022-12-26 16:42:18 +0400

    ArrayUtill.java

--- src/Homework/ArrayUtill.java
+++ src/Homework/ArrayUtill.java
@@ -18,9 +18,55 @@ public class ArrayUtill {
         System.out.println(" " + x);
         System.out.print(" " + y);
 
+        System.out.println();
+        {
+        }
+
+        int[] number = {12, 4, 33, 65, 2, 1, 7, 8, 99};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "  ");
+        }
+        System.out.println();
+
+
+        int mumber = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                if (x != 1) {
+                    System.out.print(array[i] + ", ");
+                    {
+
+                    }
+                }
+                System.out.println();
+            }
+
+
+            if (array[i] % 2 == 0) {
+                if (x != 0) ;
+                System.out.print(array[i] + " ");
+            }
+
+
+        }
+        System.out.println();
+        {
+        }
+        int qanak = 0;
+        int sum = 0;
+        int mijin = 0;
+        int[] numbers = {12, 45, 3, 9, 1, 55, 78, 31, 71};
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+            qanak = array.length;
+            mijin = sum / qanak;
+
+        }
+        System.out.println(sum);
+        System.out.println(qanak);
+        System.out.println(mijin);
+
 
     }
 }
-
-
 
