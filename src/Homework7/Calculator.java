56b6889962369e1d78bfba98f48aecc9f2e5f717
User <axachatryan877@gmail.com>
2023-01-15 15:35:51 +0400

    Example1

---
+++ src/Homework7/Calculator.java
@@ -0,0 +1,20 @@
+package Homework7;
+
+public class Calculator {
+    void Calculator() {
+
+        int x = 856;
+        int y = 254;
+        int result = x + y;
+        result = x - y;
+        result = x / y;
+        result = x * y;
+        System.out.println( x+y);
+        System.out.println( x-y);
+        System.out.println( x/y);
+        System.out.println( x*y);
+
+
+    }
+}
+
---
+++ src/Homework7/CalculatorDemo.java
@@ -0,0 +1,11 @@
+package Homework7;
+
+public class CalculatorDemo {
+    public static void main(String[] args) {
+Calculator cl=new Calculator();
+cl.Calculator();
+
+    }
+
+}
+
